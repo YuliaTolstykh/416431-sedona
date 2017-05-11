4df266b9ab5ae1271edede6d0628ec7d4caa7d6e
YuliaTolstykh <yuliatolstykh@yandex.ru>
2017-05-11 09:01:09 +0300

    Все почистила

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-var map=document.querySelector(".map");
-var GoogleMap=document.querySelector("iframe");
-var searchHotels=document.querySelector(".btn-search");
-var popup=document.querySelector(".popup-search");
+var mapImg = document.querySelector(".map img");
+var searchHotels = document.querySelector(".btn-search");
+var popup = document.querySelector(".popup-search");
 
+mapImg.classList.add("block-display");
 popup.classList.add("block-display");
 searchHotels.addEventListener("click", function (evt){
   popup.classList.toggle("block-display");
 });
 
-map.addEventListener("click", function (evt) {
-  map.classList.add("block-display");
-  GoogleMap.classList.remove("block-display");
-});
+function initialize() {
+  var mapOptions = {
+    zoom: 9,
+    center: new google.maps.LatLng(34.8731945,-111.7613001)
+  }
+  var GoogleMap = new google.maps.Map(document.querySelector(".js-map"),
+                                mapOptions);
+  var myLatLng = new google.maps.LatLng(34.8731945,-111.7613001);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: GoogleMap,
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
